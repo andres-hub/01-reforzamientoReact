ca0bbc74f1a07ef8562abdedadffffcd0b73dbb0
AndresCL <andrescalderoncl@gmail.com>
2022-08-16 15:01:40 -0500

    ok reforzamiento

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,14 @@ import { Contador } from './components/Contador';
 import { ContadorConHook } from './components/ContadorConHook';
 import { Login } from './components/Login';
 import Usuarios from './components/Usuarios';
+import { Formularios } from './components/Formularios';
 
 const App = () => {
   return (
     <div className='mt-2'>App
       <h1>Introduccion a react</h1>
+      <hr />
+      <Formularios/>
       <hr/>
       <Usuarios/>
       <hr />
